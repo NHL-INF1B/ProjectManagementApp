7c8f8f4461ac0ba6c66af2f54f1ee77eff038795
Stefan <stefan.meijer3@student.nhlstenden.com>
2022-05-16 13:24:19 +0200

    Stefan API

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,23 +1,61 @@
-import React from 'react';
-import Styles from './Styles';
-import { View, Text, SafeAreaView } from 'react-native';
+import React from "react";
+import Styles from "./Styles";
+import { View, Text, SafeAreaView } from "react-native";
+import { Button } from "react-native-web";
 
 function LoginScreen() {
+    let test;
     
-    const onPress = () => {
-        console.log('gedrukt');
-    }
-    return (
-        <SafeAreaView style={Styles.SafeAreaView}>
-            <View style={Styles.head}>
-                <Text>logo</Text>
-            </View>
+	const sendDataToAPI = (name, email, dateOfBirth, password, confirmPassword) => {
+        try {
+            fetch("http://localhost/ProjectManagementApp/src/screens/LoginScreen/handler.php", {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    dateOfBirth: dateOfBirth,
+                    password: password,
+                    confirmPassword: confirmPassword,
+                }),
+            })
+            .then((response) => response.text()) //or json - Puts response to text
+            .then((response) => {
+                catchFeedback(response);
+                console.log("feedback => " + response);
+            });
+        } catch (error) {
+            alert(error);
+        }
+	};
 
-            <View style={Styles.login}>
-                <Text>Hier alles voor login</Text>
-            </View>
-        </SafeAreaView>
-    );
+    const catchFeedback = (test) => {
+        switch (test) {
+            case "invalid_name":
+                //do something
+                console.log('het werkt!');
+                break;
+
+            default:
+                break;
+        }
+    };
+
+	return (
+		<SafeAreaView style={Styles.SafeAreaView}>
+			<View style={Styles.head}>
+				<Text>logo</Text>
+			</View>
+
+			<View style={Styles.login}>
+				<Text>Hier alles voor login</Text>
+                <Button title="druk hier" onPress={() => sendDataToAPI("Stefan", "stefan@email.com", "2001-12-26", "welkom10", "welkom10")} />
+			</View>
+		</SafeAreaView>
+	);
 }
 
 export default LoginScreen;
