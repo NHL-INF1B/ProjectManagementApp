604404b54c888710884ab72499323c00c04ecd23
Francoiskral <francois.kral@student.nhlstenden.com>
2022-05-16 13:20:25 +0200

    CreateProject gemaakt

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
-import HourEditScreen from "../ProjectManagementApp/src/screens/HourEditScreen/HourEditScreen"
+import HourEditScreen from "../ProjectManagementApp/src/screens/HourEditScreen/HourEditScreen";
+import ProjectAanmaken from "./src/screens/ProjectAanmaken/ProjectAanmaken";
 import { View } from "react-native";
 
 export default function App() {
 	return (
-		<HourEditScreen />
+		<ProjectAanmaken />
 		
 	);
 }
