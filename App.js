62420d8c73c3040b46b282a23751d69a29451f36
Clibzz <chriscrossje2001@gmail.com>
2022-05-16 10:23:19 +0200

    Added HourEditScreen, added components customTextInput & Circle

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
+import HourEditScreen from "../ProjectManagementApp/src/screens/HourEditScreen/HourEditScreen"
+import { View } from "react-native";
 
 export default function App() {
 	return (
-		<LoginScreen />
+		<HourEditScreen />
+		
 	);
 }
---
+++ src/components/Circle/Circle.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { View, StyleSheet, TextInput } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const Circle = ({ name, size, color, style }) => {
+    return (
+        <View style={styles.circle}>
+            <MaterialCommunityIcons 
+                name={name}
+                size={size}
+                color={color}
+                style={style}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        
+    },
+    circle: {
+        width: 100,
+        height: 100,
+        borderWidth: 1,
+        borderRadius: 100,
+        borderColor: 'black',
+        backgroundColor: 'white',
+        marginLeft: 127,
+    },
+});
+export default Circle
