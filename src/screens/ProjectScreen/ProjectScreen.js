16d95dbb4c18bb67a625238a17e4463bcf28bee5
JesseOost <89914221+JesseOost@users.noreply.github.com>
2022-05-16 15:53:39 +0200

    front end of project screen and start of tiles

--- src/screens/ProjectScreen/ProjectScreen.js
+++ src/screens/ProjectScreen/ProjectScreen.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import Styles from './Styles';
 import { View, Text, SafeAreaView } from 'react-native';
+import { MaterialCommunityIcons} from '@expo/vector-icons';
 
 function ProjectScreen() {
     
-    const onPress = () => {
-        console.log('gedrukt');
-    }
     return (
         <SafeAreaView style={Styles.SafeAreaView}>
-            
+            <Button style={Styles.Tile}>
+                <MaterialCommunityIcons name="book" size={55} color="#009BAA" style={Styles.Icon}/>
+                <Text style={Styles.Text}>Teamcode</Text>
+            </Button>
+            <View style={Styles.Tile}>
+                <MaterialCommunityIcons name="calendar" size={55} color="#009BAA" style={Styles.Icon}/>
+                <Text style={Styles.Text}>Planning</Text>
+            </View>
+            <View style={Styles.Tile}>
+                <MaterialCommunityIcons name="clipboard" size={55} color="#009BAA" style={Styles.Icon}/>
+                <Text style={Styles.Text}>Urenverantwoording</Text>
+            </View>
+            <View style={Styles.Tile}>
+                <MaterialCommunityIcons name="exclamation-thick" size={55} color="#009BAA" style={Styles.Icon}/>
+                <Text style={Styles.Text}>Waarschuwingen</Text>
+            </View>
         </SafeAreaView>
     );
 }
--- src/screens/ProjectScreen/Styles.js
+++ src/screens/ProjectScreen/Styles.js
@@ -3,7 +3,32 @@ import { StyleSheet, Platform, StatusBar } from 'react-native';
 export default StyleSheet.create({
 	SafeAreaView: {
 		paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-		backgroundColor:  "#009BAA",
+		backgroundColor: "#009BAA",
 		flex: 1,
+		flexDirection: "row",
+		flexWrap: "wrap",
+		justifyContent: "center",
+	},
+	Tile: {
+		minWidth: 135,
+		maxWidth: 135,
+		minHeight: 141,
+		maxHeight: 141,
+		backgroundColor: "white",
+		borderColor: "#707070",
+		borderRadius: 10,
+		flex: 1,
+		alignItems: "center",
+		margin: 15,
+	},
+	Icon: {
+		flex: 1,
+	},
+	Text: {
+		flex: 2,
+		color: "#009BAA",
+		fontWeight: "bold",
+		textAlign: "center",
+		fontSize: 25,
 	},
 });
