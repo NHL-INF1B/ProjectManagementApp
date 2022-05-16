2d30b245d28cdf75f3163e06973c48e0f0256756
FrancaBaars <franca.baars@student.nhlstenden.com>
2022-05-16 15:49:15 +0200

    De variabelen opslaan

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React from "react";
 import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
 import HourEditScreen from "../ProjectManagementApp/src/screens/HourEditScreen/HourEditScreen"
 import { View } from "react-native";
+import Registration from "./src/screens/Registration/Registration";
 
 export default function App() {
 	return (
-		<HourEditScreen />
+		<Registration />
 		
 	);
 }
