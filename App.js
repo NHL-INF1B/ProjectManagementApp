2f821ba7f66acae041d1c81a8fc63fbca8d12681
Francoiskral <francois.kral@student.nhlstenden.com>
2022-05-17 10:21:59 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
 import HourEditScreen from "../ProjectManagementApp/src/screens/HourEditScreen/HourEditScreen";
-import ProjectAanmaken from "./src/screens/ProjectAanmaken/ProjectAanmaken";
+import CreateProject from "./src/screens/CreateProject/CreateProject";
 import { View } from "react-native";
 
 export default function App() {
 	return (
-		<ProjectAanmaken />
+		<CreateProject />
 		
 	);
 }
