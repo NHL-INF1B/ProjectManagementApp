69b6ff035a3cf20a37ac3328a3fb9b7becd14752
JesseOost <89914221+JesseOost@users.noreply.github.com>
2022-05-17 15:14:07 +0200

    start of being able to press on a button

--- src/screens/ProjectScreen/ProjectScreen.js
+++ src/screens/ProjectScreen/ProjectScreen.js
@@ -7,12 +7,6 @@ export default function ProjectScreen() {
     
     return (
         <SafeAreaView style={Styles.SafeAreaView}>
-            {/* <TouchableOpacity onPress={() => navigation.navigate("../LoginScreen/LoginScreen")}>
-                <View style={Styles.Tile}>
-                    <MaterialCommunityIcons name="book" size={55} color="#009BAA" style={Styles.Icon}/>
-                    <Text style={Styles.Text}>Teamcode</Text>
-                </View>
-            </TouchableOpacity> */}
             <Tile text="Teamcode" image="book" path="../LoginScreen/LoginScreen" />
             <Tile text="Planning" image="calendar" path="../LoginScreen/LoginScreen" />
             <Tile text="Urenverwantwoording" image="clipboard" path="../LoginScreen/LoginScreen" />
@@ -20,18 +14,6 @@ export default function ProjectScreen() {
             <Tile text="Uitnodigingen" image="account-plus" path="../LoginScreen/LoginScreen" />
             <Tile text="Leden" image="account-group" path="../LoginScreen/LoginScreen" />
             <Tile text="Scorebord" image="star" path="../LoginScreen/LoginScreen" />
-            {/* <View style={Styles.Tile}>
-                <MaterialCommunityIcons name="calendar" size={55} color="#009BAA" style={Styles.Icon}/>
-                <Text style={Styles.Text}>Planning</Text>
-            </View>
-            <View style={Styles.Tile}>
-                <MaterialCommunityIcons name="clipboard" size={55} color="#009BAA" style={Styles.Icon}/>
-                <Text style={Styles.Text}>Urenverantwoording</Text>
-            </View>
-            <View style={Styles.Tile}>
-                <MaterialCommunityIcons name="exclamation-thick" size={55} color="#009BAA" style={Styles.Icon}/>
-                <Text style={Styles.Text}>Waarschuwingen</Text>
-            </View> */}
         </SafeAreaView>
     );
 }
