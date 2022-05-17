86c92dc8e354dbeaff55907cd277380bb02ab345
FrancaBaars <franca.baars@student.nhlstenden.com>
2022-05-17 10:56:01 +0200

    Revert "De variabelen opslaan"
    This reverts commit 2d30b245d28cdf75f3163e06973c48e0f0256756.

--- App.js
+++ App.js
@@ -2,11 +2,10 @@ import React from "react";
 import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
 import HourEditScreen from "../ProjectManagementApp/src/screens/HourEditScreen/HourEditScreen"
 import { View } from "react-native";
-import Registration from "./src/screens/Registration/Registration";
 
 export default function App() {
 	return (
-		<Registration />
+		<HourEditScreen />
 		
 	);
 }
