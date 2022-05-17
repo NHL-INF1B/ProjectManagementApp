345abdb01aef0bab1a51db25cb98b92da6e1ea94
JesseOost <89914221+JesseOost@users.noreply.github.com>
2022-05-17 14:54:15 +0200

    created more tiles

--- src/screens/ProjectScreen/ProjectScreen.js
+++ src/screens/ProjectScreen/ProjectScreen.js
@@ -14,6 +14,12 @@ export default function ProjectScreen() {
                 </View>
             </TouchableOpacity> */}
             <Tile text="Teamcode" image="book" path="../LoginScreen/LoginScreen" />
+            <Tile text="Planning" image="calendar" path="../LoginScreen/LoginScreen" />
+            <Tile text="Urenverwantwoording" image="clipboard" path="../LoginScreen/LoginScreen" />
+            <Tile text="Waarschuwingen" image="exclamation-thick" path="../LoginScreen/LoginScreen" />
+            <Tile text="Uitnodigingen" image="account-plus" path="../LoginScreen/LoginScreen" />
+            <Tile text="Leden" image="account-group" path="../LoginScreen/LoginScreen" />
+            <Tile text="Scorebord" image="star" path="../LoginScreen/LoginScreen" />
             {/* <View style={Styles.Tile}>
                 <MaterialCommunityIcons name="calendar" size={55} color="#009BAA" style={Styles.Icon}/>
                 <Text style={Styles.Text}>Planning</Text>
