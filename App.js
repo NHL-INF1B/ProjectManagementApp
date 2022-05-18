5161f90ec5226f4328477c435dafdb7fb5f210ee
BartHerms <91067528+BartHerms@users.noreply.github.com>
2022-05-18 10:01:17 +0200

    Je kan nu data in de database zetten
    Je kan nu data in de database zetten

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React from "react";
 import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
 import HourEditScreen from "../ProjectManagementApp/src/screens/HourEditScreen/HourEditScreen"
 import { View } from "react-native";
+import Planning from "./src/screens/Planning/Planning";
 
 export default function App() {
 	return (
-		<HourEditScreen />
+		<Planning />
 		
 	);
 }
--- src/screens/Planning/Planning.js
+++ src/screens/Planning/Planning.js
@@ -1,4 +1,4 @@
-import { Text, ScrollView, Pressable } from 'react-native';
+import { Text, ScrollView, Pressable, View, Button } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import React, { useState } from 'react';
 import CustomTextInput from '../../components/CustomTextInput/CustomTextInput';
@@ -8,6 +8,34 @@ import Circle from '../../components/Circle/Circle';
 const Planning = () => {
   const [Activiteit, setActiviteit] = useState('');
   const [weeknummer, setWeeknummer] = useState('');
+
+
+  const sendDataToAPI = (planning, week, activiteit, project_id) => {
+    try {
+        fetch("http://localhost:8080/PmaAPI/handlers/planning/planningHandler.php", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                planning: planning,
+                week: week,
+                activiteit: activiteit,
+                project_id: project_id,
+            }),
+        })
+        .then((response) => response.text())
+        // .then((response) => response.json())
+        .then((response) => {
+            console.log(response);
+            // catchFeedback(response);
+        });
+    } catch (error) {
+        alert(error);
+    }
+};
+
   return (
     <ScrollView style={styles.root}>
       <MaterialCommunityIcons style={styles.arrow}  name="arrow-left-thick" size={60} color={'black'} />
@@ -20,6 +48,9 @@ const Planning = () => {
       <Pressable>
           <Text style={[styles.button, styles.buttonBlue, styles.marginTop25, styles.marginBottom25]}>Toevoegen</Text>
       </Pressable>
+      <View>
+        <Button title="druk hier" onPress={() => sendDataToAPI("planning", "week", "activiteit", "project_id")} />
+      </View>
 
       {/* <Button
         title="Toevoegen"
