929efe07b7c63fb9008322ec24102441407ec122
Stefan <stefan.meijer3@student.nhlstenden.com>
2022-05-20 13:15:27 +0200

    test

--- src/screens/RegisterScreen/RegisterScreen.js
+++ src/screens/RegisterScreen/RegisterScreen.js
@@ -1,105 +1,189 @@
-import React from "react";
+import { React, useState } from "react";
 import Styles from "./Styles";
-import { View, Text, SafeAreaView } from "react-native";
-import { Button } from "react-native-web";
+import { View, Text, ScrollView, Button, TextInput, Pressable, Image, SafeAreaView } from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import CustomTextInput from "../../components/CustomTextInput/CustomTextInput";
 
-const RegisterScreen = ({ navigation, route }) => {
-	const sendDataToAPI = (name, email, dateOfBirth, password, confirmPassword) => {
-        try {
-            fetch("http://localhost/pma/PmaAPI/handlers/registration/registrationHandler.php", {
-                method: "POST",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    name: name,
-                    email: email,
-                    dateOfBirth: dateOfBirth,
-                    password: password,
-                    confirmPassword: confirmPassword,
-                }),
-            })
-            // .then((response) => response.text())
-            .then((response) => response.json())
-            .then((response) => {
-                // console.log(response);
-                catchFeedback(response);
-            });
-        } catch (error) {
-            alert(error);
-        }
-	};
-
-    /**
-     * Function to catch feedback of request to API
-     * @param response JSON object of response
-     */
-    const catchFeedback = (response) => {
-        
-        // console.log(response);
-        // console.log(response.length);
-
-        for (let index = 0; index < response.length; index++) {
-            switch (response[index]) {
-                case 'name_incorrect':
-                    //do something
-                    console.log('name_incorrect');
-                    break;
-
-                case 'email_incorrect':
-                    //do something
-                    console.log('email_incorrect');
-                    break;
-
-                case 'dateOfBirth_incorrect':
-                    //do something
-                    console.log('dateOfBirth_incorrect');
-                    break;
-
-                case 'password_incorrect':
-                    //do something
-                    console.log('password_incorrect');
-                    break;
-
-                case 'confirmPassword_incorrect':
-                    //do something
-                    console.log('confirmPassword_incorrect');
-                    break;
-
-                case 'samePassword_incorrect':
-                    //do something
-                    console.log('samePassword_incorrect');
-                    break;
-
-                case 'email_in_use':
-                    //do something
-                    console.log('email_in_use');
-                    break;
-                
-                default:
-                    //Succes
-                    console.log(response[0].id);
-                    console.log(response[0].name);
-                    console.log(response[0].email);
-                    console.log(response[0].dateOfBirth);
-                    break;
-            }
-        }
+const RegisterScreen = ({ navigation }) => {
+    //form handling
+    const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const DATE_REGEX = /^\d{4}-\d{2}-\d{2}$/;
+    const PASS_REGEX = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,254}$/;
+    const { control, handleSubmit, formState: { errors } } = useForm({
+      defaultValues: {
+        name: '',
+        email: '',
+        dateOfBirth: '',
+        password: '',
+      }
+    });
+    const onSubmit = (data) => {
+      console.log(data)
     };
 
-	return (
-		<SafeAreaView style={Styles.SafeAreaView}>
-			<View style={Styles.head}>
-				<Text>logo</Text>
-			</View>
+    const sendDataToAPI = (name, email, dateOfBirth, password, confirmPassword) => {
+          try {
+              fetch("http://localhost/pma/PmaAPI/handlers/registration/registrationHandler.php", {
+                  method: "POST",
+                  headers: {
+                      Accept: "application/json",
+                      "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                      name: name,
+                      email: email,
+                      dateOfBirth: dateOfBirth,
+                      password: password,
+                      confirmPassword: confirmPassword,
+                  }),
+              })
+              // .then((response) => response.text())
+              .then((response) => response.json())
+              .then((response) => {
+                  // console.log(response);
+                  catchFeedback(response);
+              });
+          } catch (error) {
+              alert(error);
+          }
+    };
 
-			<View style={Styles.login}>
-				<Text>Hier alles voor registratie</Text>
-                <Button title="druk hier" onPress={() => sendDataToAPI("Stefan", "stefan@email.com", "2001-12-26", "!Welkom10", "!Welkom10")} />
-			</View>
-		</SafeAreaView>
-	);
-}
+    const catchFeedback = (response) => {
+      // console.log(response);
+  
+      switch (response[0]) {
+        case "name_incorrect":
+          alert("De naam is verkeerd.");
+          break;
+        case "email_incorrect":
+          alert("Het emailadres is verkeerd.");
+          break;
+        case "dateOfBirth_incorrect":
+          alert("De geboortedatum is verkeerd.");
+          break;
+        case "password_incorrect":
+          alert("Het eerste wachtwoord is verkeerd.");
+          break;
+        case "confirmPassword_incorrect":
+          alert("Het tweede wachtwoord is verkeerd.");
+          break;
+        case "samePassword_incorrect":
+          alert("De wachtwoorden zijn niet hetzelfde.");
+          break;
+        case "email_in_use":
+          alert("Dit emailadres is al in gebruik.");
+          break;
+        default:
+          alert("Je account is aangemaakt, log nu in met je gegevens.");
+          navigation.navigate('LoginScreen');
+      }
+    };
+
+
+    return (
+        <SafeAreaView style={Styles.SafeAreaView}>
+          <ScrollView style={Styles.container}>
+            <View>
+              <View style={Styles.inputContainer}>
+                <Controller
+                  name="name"
+                  control={control}
+                  rules={{
+                    required: { value: true, message: 'Naam is verplicht' },
+                  }}
+                  render={({ field: { onChange, value } }) => (
+                    <CustomTextInput 
+                      placeholder="Naam" 
+                      placeholderTextColor="#707070" 
+                      onChangeText={(text) => onChange(text)} 
+                      value={value} 
+                      errorText={errors?.name?.message} 
+                      titleText="naam"
+                    />
+                  )}
+                />
+              </View>
+
+              <View style={Styles.inputContainer}>
+                <Controller
+                  name="email"
+                  control={control}
+                  rules={{
+                    required: { value: true, message: 'Email is verplicht' },
+                    pattern: {
+                      value: EMAIL_REGEX,
+                      message: 'Email is niet correct'
+                    },
+                  }}
+                  render={({ field: { onChange, value } }) => (
+                    <CustomTextInput 
+                      placeholder="Emailadres" 
+                      placeholderTextColor="#707070" 
+                      onChangeText={(text) => onChange(text)} 
+                      value={value} 
+                      errorText={errors?.email?.message} 
+                      titleText="email"
+                    />
+                  )}
+                />
+              </View>
+
+              <View style={Styles.inputContainer}>
+                <Controller
+                  name="dateOfBirth"
+                  control={control}
+                  rules={{
+                    required: { value: true, message: 'Geboortedatum is verplicht' },
+                    pattern: {
+                      value: DATE_REGEX,
+                      message: 'Geboortedatum is niet correct'
+                    },
+                  }}
+                  render={({ field: { onChange, value } }) => (
+                    <CustomTextInput
+                      placeholder="Geboortedatum" 
+                      placeholderTextColor="#707070" 
+                      onChangeText={(text) => onChange(text)} 
+                      value={value} 
+                      errorText={errors?.dateOfBirth?.message} 
+                      titleText="geboortedatum"
+                    />
+                  )}
+                />
+              </View>
+
+              <View style={Styles.inputContainer}>
+                <Controller
+                  name="password"
+                  control={control}
+                  rules={{
+                    required: { value: true, message: 'Wachtwoord is verplicht' },
+                    pattern: {
+                      value: PASS_REGEX,
+                      message: 'Wachtwoord is te zwak'
+                    },
+                  }}
+                  render={({ field: { onChange, value } }) => (
+                    <CustomTextInput 
+                      placeholder="Wachtwoord" 
+                      placeholderTextColor="#707070" 
+                      onChangeText={(text) => onChange(text)} 
+                      value={value} 
+                      errorText={errors?.password?.message} 
+                      secureTextEntry={true}
+                      titleText="wachtwoord"
+                    />
+                  )}
+                />
+              </View>
+            </View>
+
+            <View>
+              <Button style={Styles.button} title="Submit" onPress={handleSubmit(onSubmit)} />
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      );
+};
 
 export default RegisterScreen;
