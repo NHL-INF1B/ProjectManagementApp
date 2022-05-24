21ec757401d521a6a208eb0436f9bf04b46480b1
BartHerms <91067528+BartHerms@users.noreply.github.com>
2022-05-24 10:14:58 +0200

    Opzetje ShowPlanning
    Opzetje ShowPlanning

--- App.js
+++ App.js
@@ -4,63 +4,65 @@ import LoginScreen from "./src/screens/LoginScreenNew/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen/RegisterScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ShowPlanning from "./src/screens/ShowPlanning/ShowPlanning";
 
-const Stack = createNativeStackNavigator();
+const Stack = ShowPlanning();
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator screenOptions={{ headerShown: false }}>
-			<Stack.Screen
-					name="RegisterScreen"
-					component={RegisterScreen}
-				/>
-				<Stack.Screen
-					name="LoginScreen"
-					component={LoginScreen}
-				/>
+		<ShowPlanning />
+		// <NavigationContainer>
+		// 	<Stack.Navigator screenOptions={{ headerShown: false }}>
+		// 	<Stack.Screen
+		// 			name="RegisterScreen"
+		// 			component={RegisterScreen}
+		// 		/>
+		// 		<Stack.Screen
+		// 			name="LoginScreen"
+		// 			component={LoginScreen}
+		// 		/>
 
-				<Stack.Screen
-					name="ProjectScreen"
-					component={ProjectScreen}
-				/>
+		// 		<Stack.Screen
+		// 			name="ProjectScreen"
+		// 			component={ProjectScreen}
+		// 		/>
 
-				<Stack.Screen
-					name="TeamcodeScreen"
-					component={""}
-				/>
+		// 		<Stack.Screen
+		// 			name="TeamcodeScreen"
+		// 			component={""}
+		// 		/>
 
-				<Stack.Screen
-					name="PlanningScreen"
-					component={""}
-				/>
+		// 		<Stack.Screen
+		// 			name="PlanningScreen"
+		// 			component={""}
+		// 		/>
 
-				<Stack.Screen
-					name="LogbookScreen"
-					component={""}
-				/>
+		// 		<Stack.Screen
+		// 			name="LogbookScreen"
+		// 			component={""}
+		// 		/>
 
-				<Stack.Screen
-					name="WarningScreen"
-					component={""}
-				/>
+		// 		<Stack.Screen
+		// 			name="WarningScreen"
+		// 			component={""}
+		// 		/>
 
-				<Stack.Screen
-					name="InviteScreen"
-					component={""}
-				/>
+		// 		<Stack.Screen
+		// 			name="InviteScreen"
+		// 			component={""}
+		// 		/>
 
-				<Stack.Screen
-					name="MemberScreen"
-					component={""}
-				/>
+		// 		<Stack.Screen
+		// 			name="MemberScreen"
+		// 			component={""}
+		// 		/>
 
-				<Stack.Screen
-					name="ScoreScreen"
-					component={""}
-				/>
+		// 		<Stack.Screen
+		// 			name="ScoreScreen"
+		// 			component={""}
+		// 		/>
 
-			</Stack.Navigator>
-		</NavigationContainer>
+		// 	</Stack.Navigator>
+		// </NavigationContainer>
 	);
 }
