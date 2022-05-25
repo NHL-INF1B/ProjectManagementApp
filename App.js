acb8b53b1b2a5f6a74d7bdf42d9eab3ed41a5211
BartHerms <91067528+BartHerms@users.noreply.github.com>
2022-05-25 11:01:10 +0200

    Kleine aanpassing aan app.js zodat mn pagina het doet
    Kleine aanpassing aan app.js zodat mn pagina het doet

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {React, useEffect} from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ScheduleEditScreen from "./src/screens/ScheduleEditScreen/ScheduleEditScreen";
+import Planning from "./src/screens/Planning/Planning";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,59 +13,10 @@ export default function App() {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator screenOptions={{ headerShown: false }}>
-				<Stack.Screen
-					name="ScheduleEditScreen"
-					component={ScheduleEditScreen}
+			<Stack.Screen
+					name="Planning"
+					component={Planning}
 				/>
-
-				<Stack.Screen
-					name="LoginScreen"
-					component={LoginScreen}
-				/>
-
-				<Stack.Screen
-					name="RegisterScreen"
-					component={RegisterScreen}
-				/>
-
-
-				<Stack.Screen
-					name="ProjectScreen"
-					component={ProjectScreen}
-				/>
-
-				
-				<Stack.Screen
-					name="TeamcodeScreen"
-					component={""}
-				/>
-
-				
-
-				<Stack.Screen
-					name="LogbookScreen"
-					component={""}
-				/>
-
-				<Stack.Screen
-					name="WarningScreen"
-					component={""}
-				/>
-
-				<Stack.Screen
-					name="InviteScreen"
-					component={""}
-				/>
-
-				<Stack.Screen
-					name="MemberScreen"
-					component={""}
-				/>
-
-				<Stack.Screen
-					name="ScoreScreen"
-					component={""}
-				/> */}
 
 			</Stack.Navigator>
 		</NavigationContainer>
