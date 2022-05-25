8c45e48d08394956da56d5158e8dabed99dce3dc
FrancaBaars <franca.baars@student.nhlstenden.com>
2022-05-25 16:23:37 +0200

    Get member info toegevoegd

--- src/screens/MemberInfo/MemberInfo.js
+++ src/screens/MemberInfo/MemberInfo.js
@@ -7,35 +7,59 @@ import Circle from "../../components/Circle/Circle";
 import ShowProfileInfo from "../../components/TextLatenZien/ShowProfileInfo";
 
 const MemberInfo = () => {
+  const memberId = 10; //temporary until the members page is made.
   const [name, setName] = useState("-");
   const [email, setEmail] = useState("-");
   const [dateOfBirth, setDateOfBirth] = useState("-");
   const [phoneNumber, setPhoneNumber] = useState("-");
   const [discord, setDiscord] = useState("-");
 
-  const getData = async () => {
-    try {
-      const jsonValue = await AsyncStorage.getItem("@user_data");
-      if (jsonValue !== null) {
-        return JSON.parse(jsonValue);
-      }
-    } catch (e) {
-      alert(e);
-    }
-  };
+  // const getData = async () => {
+  //   try {
+  //     const jsonValue = await AsyncStorage.getItem("@member_data");
+  //     if (jsonValue !== null) {
+  //       return JSON.parse(jsonValue);
+  //     }
+  //   } catch (e) {
+  //     alert(e);
+  //   }
+  // };
 
   useEffect(() => {
-    const data = getData();
-    data.then((data) => {
-      if (data !== undefined) {
-        setName(data["name"]),
-          setEmail(data["email"]),
-          setDateOfBirth(data["dateOfBirth"]),
-          setPhoneNumber(data["phoneNumber"]),
-          setDiscord(data["discord"]);
-      }
-    });
+    // const data = getData();
+    getMemberInfo(memberId);
+    console.log(memberId);
   }, []);
+
+  const getMemberInfo = (memberId) => {
+    try {
+      fetch(
+        "http://localhost/PMA/PmaAPI/handlers/memberInfoHandler/memberInfoHandler.php",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            memberId: memberId,
+          }),
+        }
+      )
+        .then((response) => response.text())
+        // .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+          // setName(response.name);
+          // setEmail(response.email);
+          // setDateOfBirth(response.dateOfBirth);
+          // setPhoneNumber(response.phoneNumber);
+          // setDiscord(response.discord);
+        });
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   return (
     <SafeAreaView style={Styles.SafeAreaView}>
@@ -46,31 +70,31 @@ const MemberInfo = () => {
         name={name}
         iconName="account"
         iconSize={30}
-        iconColor="black"
+        iconColor="#009BAA"
       />
       <ShowProfileInfo
         name={email}
         iconName="email"
         iconSize={30}
-        iconColor="black"
+        iconColor="#009BAA"
       />
       <ShowProfileInfo
         name={dateOfBirth}
         iconName="calendar"
         iconSize={30}
-        iconColor="black"
+        iconColor="#009BAA"
       />
       <ShowProfileInfo
         name={phoneNumber}
         iconName="phone"
         iconSize={30}
-        iconColor="black"
+        iconColor="#009BAA"
       />
       <ShowProfileInfo
         name={discord}
         iconName="discord"
         iconSize={30}
-        iconColor="black"
+        iconColor="#5865F2"
       />
     </SafeAreaView>
   );
