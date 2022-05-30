1f0ea2b088b59e97955407236d75a8f7fc7ec07e
Francoiskral <francois.kral@student.nhlstenden.com>
2022-05-30 11:14:08 +0200

    Teamcode toevoegen bestand toegevoegd

---
+++ src/screens/TeamcodeToevoegen/TeamcodeToevoegen.js
@@ -0,0 +1,67 @@
+'use strict';
+
+import { Text, ScrollView, View, Pressable, Button } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import styles from './Styles';
+import Circle from '../../components/Circle/Circle';
+import { useForm, Controller } from "react-hook-form";
+import CustomTextInput from '../../components/CustomTextInput/CustomTextInput';
+
+  const CreateProject = ({ navigation }) => {
+    //form handling
+    // const NAME_REGEX = /^[a-zA-Z0-9 ]{3,30}$/;
+    // const { control, handleSubmit, formState: { errors } } = useForm({
+    //   defaultValues: {
+    //     name: '',
+    //   }
+    // });
+    const onSubmit = (data) => {
+      sendDataToAPI(data.name);
+      console.log(data);
+    };
+
+  //the things where the info goes in.
+  // const [ProjectNaam, setProjectNaam] = useState('');
+
+  const sendDataToAPI = (ProjectNaam) => {
+    try {
+        fetch("http://localhost/pma/PmaAPI/handlers/createProject/createProjectHandler.php", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: ProjectNaam,
+            }),
+        })
+        .then((response) => response.json())
+        .then((response) => {
+            // console.log(response);
+            catchFeedback(response);
+        });
+    } catch (error) {
+        alert(error);
+    }
+};
+
+  //the screen
+return (
+    <ScrollView style={styles.root}>
+        <MaterialCommunityIcons style={styles.arrow}  name="arrow-left-thick" size={60} color={'black'} />
+        <View style={styles.div}>
+        <Circle name={"account-group"} size={60} color={"black"} style={[styles.icon,]} />
+        </View>
+        <View style={styles.div}>
+        <Text style={[styles.title, styles.marginBottom25, styles.marginTop50, styles.sampleText,]}>PROJECT AANMAKEN</Text>
+        </View>
+
+        <Pressable onPress={handleSubmit(onSubmit)}>
+        <Text style={[styles.button, styles.buttonBlue, styles.marginTop25, styles.marginBottom25]}>AANMAKEN</Text>
+        </Pressable>
+    </ScrollView>
+    );
+};
+  
+export default CreateProject;
