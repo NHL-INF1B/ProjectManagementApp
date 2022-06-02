0576243348eebf051360737385f2333881b137ea
Clibzz <chriscrossje2001@gmail.com>
2022-06-02 14:04:24 +0200

    Fixed houreditscreen so it functions properly

--- src/screens/HourEditScreen/HourEditScreen.js
+++ src/screens/HourEditScreen/HourEditScreen.js
@@ -1,20 +1,67 @@
-import { Text, ScrollView, View } from 'react-native';
+import { Text, SafeAreaView, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import React, { useState } from 'react';
-import CustomTextInputOld from '../../components/CustomTextInput/CustomTextInputOld';
+import React, { useState, useEffect } from 'react';
+import CustomTextInput from '../../components/CustomTextInput/CustomTextInput';
 import styles from './Styles';
 import Circle from '../../components/Circle/Circle';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useForm, Controller } from "react-hook-form";
+import CustomButton from '../../components/CustomButton/CustomButton';
+import { NavigationContainer } from '@react-navigation/native';
 
 
-const HourEditScreen = () => {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [date, setDate] = useState('');
-    const [time_start, setTime_Start] = useState('');
-    const [time_end, setTime_End] = useState('');
+const HourEditScreen = ({navigation}) => {
 
-    const editActivity = (title, description, date, time_start, time_end) => {
+    const [data, setData] = useState([]);
+    const { control, handleSubmit, formState: { errors }, getValues, setValue } = useForm({
+        defaultValues: {
+            title: "",
+            description: "",
+            date: "",
+            time_start: "",
+            time_end: "",
+        }
+    });
+    
+    const id = 1;
+    useEffect(() => {
+        readData(id);
+    }, []);
+
+    const updateData = (data) => {
+        editActivity(data);
+        readData(id);
+        alert("De gegevens zijn succesvol aangepast");
+    };
+
+    const deleteData = (data) => {
+        deleteActivity(data);
+        readData(id);
+    };
+    
+    const readData = (id) => {
+        fetch('http://localhost/ReactNativeAPI/PmaAPI/handlers/houredit/houreditSelectHandler.php', {
+            method: 'post',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: id,
+            })
+        })
+        .then((response) => response.json())
+        .then((response) => {
+            // console.log(response);
+            setValue("title", response.title);
+            setValue("description", response.description);
+            setValue("date", response.date);
+            setValue("time_start", response.time_start);
+            setValue("time_end", response.time_end);
+        })
+    };
+
+    const editActivity = (data) => {
         try {
             fetch("http://localhost/ReactNativeAPI/PmaAPI/handlers/houredit/houreditUpdateHandler.php", {
                 method: "POST",
@@ -23,11 +70,12 @@ const HourEditScreen = () => {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
-                    title: title,
-                    description: description,
-                    date: date,
-                    time_start: time_start,
-                    time_end: time_end,
+                    id: id,
+                    title: data.title,
+                    description: data.description,
+                    date: data.date,
+                    time_start: data.time_start,
+                    time_end: data.time_end,
                 }),
             })
             .then((response) => response.json())
@@ -40,25 +88,23 @@ const HourEditScreen = () => {
         }
     };
 
-    const deleteActivity = (title, description, date, time_start, time_end) => {
+    const deleteActivity = (data) => {
         try {
             fetch("http://localhost/ReactNativeAPI/PmaAPI/handlers/houredit/houreditDeleteHandler.php", {
-                method: "DELETE",
+                method: "POST",
                 headers: {
                     Accept: "application/json",
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
-                    title: title,
-                    description: description,
-                    date: date,
-                    time_start: time_start,
-                    time_end: time_end,
+                    id: id,
                 }),
             })
-            .then((response) => response.json())
+            .then((response) => response.text())
             .then((response) => {
                 console.log(response);
+                alert("De gegevens zijn succesvol verwijderd");
+                navigation.navigate("HourAddScreen");
                 // catchFeedback(response);
             });
         } catch (error) {
@@ -68,35 +114,158 @@ const HourEditScreen = () => {
     
 
     return (
-        <ScrollView style={styles.root}>
-            <MaterialCommunityIcons style={styles.arrow}  name="" size={60} color={'black'} />
-            <Circle name={"alert-circle"} size={60} color={"#000000"} style={styles.icon} />
-            <Text style={[styles.title, styles.marginBottom5]}>Urenverantwoording{"\n"}bewerken</Text>
-            <Text style={styles.subtitle}>Activiteit</Text>
-            <CustomTextInputOld placeholder="Activiteit" value={title} setValue={setTitle} />
-            <Text style={styles.subtitle}>Beschrijving</Text>
-            <CustomTextInputOld placeholder="Beschrijving" value={description} setValue={setDescription} />
-            <Text style={styles.subtitle}>Datum</Text>
-            <CustomTextInputOld placeholder="Datum" value={date} setValue={setDate} />
-            <Text style={styles.subtitle}>Start Tijd</Text>
-            <CustomTextInputOld placeholder="Start Tijd" value={time_start} setValue={setTime_Start} />
-            <Text style={styles.subtitle}>Eind Tijd</Text>
-            <CustomTextInputOld style={styles.marginBottom5} placeholder="Eind Tijd" value={time_end} setValue={setTime_End} />
+        <SafeAreaView style={styles.SafeAreaView}>
+            <View>
+                <Circle name={"clipboard-text"} size={60} color={"#000000"} text={"Urenverantwoording\nBewerken"} />
+            </View>
 
-            <TouchableOpacity 
-                style={[styles.button, styles.buttonBlue, styles.marginTop1]} 
-                onPress={() => editActivity(title, description, date, time_start, time_end)}
-                activeOpacity={0.6}>
-                <Text styles={styles.subtitle}>BEWERKEN</Text>
-            </TouchableOpacity>
+            <View>
+                <Controller
+                    name="title"
+                    control={control}
+                    rules={{
+                        required: { value: true, message: 'Activiteit is verplicht' },
+                        maxLength: {
+                            value: 50,
+                            message: 'Maximaal 50 tekens lang',
+                        }
+                    }}
+                    render={({ field: { onChange, value } }) => (
+                        <CustomTextInput 
+                            placeholder="Activiteit" 
+                            onChangeText={(text) => onChange(text)} 
+                            value={value} 
+                            errorText={errors?.title?.message} 
+                            titleText="Activiteit"
+                        />
+                    )}
+                />
+            </View>
 
-            <TouchableOpacity 
-                style={[styles.button, styles.buttonRed, styles.marginTop1]} 
-                onPress={() => deleteActivity(title, description, date, time_start, time_end)}
+            <View style={styles.marginBottom1}>
+                <Controller
+                    name="description"
+                    control={control}
+                    rules={{
+                        required: { value: true, message: 'Beschrijving is verplicht' },
+                        maxLength: {
+                            value: 50,
+                            message: 'Maximaal 50 tekens lang',
+                        }
+                    }}
+                    render={({ field: { onChange, value } }) => (
+                        <CustomTextInput 
+                            placeholder="Description" 
+                            onChangeText={(text) => onChange(text)} 
+                            value={value} 
+                            errorText={errors?.description?.message} 
+                            titleText="Description"
+                        />
+                    )}
+                />
+            </View>
+
+            <View>
+                <Controller
+                    name="date"
+                    control={control}
+                    rules={{
+                        required: { value: true, message: 'Datum is verplicht' },
+                        maxLength: {
+                            value: 50,
+                            message: 'Maximaal 50 tekens lang',
+                        }
+                    }}
+                    render={({ field: { onChange, value } }) => (
+                        <CustomTextInput 
+                            placeholder="Datum" 
+                            onChangeText={(text) => onChange(text)} 
+                            value={value} 
+                            errorText={errors?.date?.message} 
+                            titleText="Datum"
+                        />
+                    )}
+                />
+            </View>
+
+            <View style={styles.marginBottom1}>
+                <Controller
+                    name="time_start"
+                    control={control}
+                    rules={{
+                        required: { value: true, message: 'Start tijd is verplicht' },
+                        maxLength: {
+                            value: 50,
+                            message: 'Maximaal 50 tekens lang',
+                        }
+                    }}
+                    render={({ field: { onChange, value } }) => (
+                        <CustomTextInput 
+                            placeholder="Time_start" 
+                            onChangeText={(text) => onChange(text)} 
+                            value={value} 
+                            errorText={errors?.time_start?.message} 
+                            titleText="Time_start"
+                        />
+                    )}
+                />
+            </View>
+
+            <View style={styles.marginBottom1}>
+                <Controller
+                    name="time_end"
+                    control={control}
+                    rules={{
+                        required: { value: true, message: 'Stop tijd is verplicht' },
+                        maxLength: {
+                            value: 50,
+                            message: 'Maximaal 50 tekens lang',
+                        }
+                    }}
+                    render={({ field: { onChange, value } }) => (
+                        // renderItem={({ item }) => 
+                        <CustomTextInput 
+                            data={data}
+                            placeholder="Time_end" 
+                            onChangeText={(text) => onChange(text)} 
+                            value={value} 
+                            errorText={errors?.time_end?.message} 
+                            titleText="Time_end"
+                        />
+                    )}
+                />
+            </View>
+
+            <View style={styles.marginBottom1}>
+                <CustomButton 
+                    buttonType={"blueButton"}
+                    text={"Bewerken"}
+                    onPress={handleSubmit(updateData)}
+                />
+            </View>
+
+            <View>
+                <CustomButton 
+                    buttonType={"redButton"}
+                    text={"Verwijderen"}
+                    onPress={handleSubmit(deleteData)}
+                />
+            </View>
+
+            {/* <TouchableOpacity 
+                style={[styles.button, styles.buttonGreen]} 
+                onPress={() => startTimer(title, description, date, time_start)}
                 activeOpacity={0.6}>
-                <Text styles={styles.subtitle}>VERWIJDEREN</Text>
+                <Text>START</Text>
             </TouchableOpacity>
-        </ScrollView>
+            <TouchableOpacity 
+                style={[styles.button, styles.buttonRed]} 
+                onPress={() => stopTimer(title, description, date, time_end)}
+                activeOpacity={0.6}>
+                <Text styles={styles.subtitle}>STOP</Text>
+            </TouchableOpacity> */}
+            
+        </SafeAreaView>
     );
 }
 
