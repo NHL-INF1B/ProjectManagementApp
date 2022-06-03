9f5b182445f3ae7964842b12921e388a9e57bc6b
JesseOost <89914221+JesseOost@users.noreply.github.com>
2022-06-03 10:22:41 +0200

    removed files that never will be used

--- src/screens/ViewMembersLogbook/Styles.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet, Platform, StatusBar } from "react-native";
-
-export default StyleSheet.create({
-  SafeAreaView: {
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    backgroundColor: "#009BAA",
-  },
-});
