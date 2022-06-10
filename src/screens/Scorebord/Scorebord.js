1caa229775640a69fdbf060827a9936534dd4ef0
FrancaBaars <franca.baars@student.nhlstenden.com>
2022-06-10 10:41:46 +0200

    Added flatlist and Styling

--- src/screens/Scorebord/Scorebord.js
+++ src/screens/Scorebord/Scorebord.js
@@ -2,12 +2,16 @@ import { React, useEffect, useState } from "react";
 import Styles from "./Styles";
 import { View, Text, SafeAreaView } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome5 } from '@expo/vector-icons'; 
+import Header from "../../components/Header/Header";
 
 
-const Scorebord = ({ navigation, route }) => {
+const Scorebord = ({ route }) => {
 
     const [projectMembers, setProjectMembers] = useState([]);
     const projectId = route.params.projectId;
+    const userId = route.params.userId;
 
     useEffect(() => {
         getScoreBordData(projectId);
@@ -25,7 +29,7 @@ const Scorebord = ({ navigation, route }) => {
 					projectId: projectId,
 				}),
 			})
-            .then((response) => response.text())
+            .then((response) => response.json())
             .then((response) => {
                 console.log(response);
                 setProjectMembers(response);
@@ -36,25 +40,42 @@ const Scorebord = ({ navigation, route }) => {
 		}
     }
 
-    const Member = ({ title }) => (
-        <View style={Styles.item}>
-          <Text style={Styles.title}>{title}</Text>
+    const Item = ({ item }) => (
+        <View style={Styles.miniContainer}>
+            <View style={Styles.iconContainer}>
+                <MaterialCommunityIcons name={"account"} size={80} color={"black"}/>
+            </View>
+
+            <View style={Styles.textContainer}>
+                <View>
+                    <Text style={Styles.name}>{item.name}</Text>
+                </View>
+                <View style={Styles.pointsContainer}>
+                    <FontAwesome5 name="medal" size={24} color="black" />
+                    <Text style={Styles.points}>{item.reward_points}</Text>
+                </View>
+            </View>
         </View>
       );
 
+      const renderItem = ({ item }) => {
+    
+        return (
+          <Item
+            item={item}
+          />
+        );
+      };
+
 
     return (
-        <SafeAreaView>
-            {/* <FlatList
-                data={projectMembers}
-                keyExtractor={(projectMember) => projectMember.id.toString()}
-                renderItem={({ item }) => 
-                    <Member 
-                        person={item.user_id} 
-                        reason={item.reason} 
-                    />
-                }
-            /> */}
+        <SafeAreaView style={Styles.SafeAreaView}>
+            <Header GoToType="None" GoTo="None" CenterGoTo="None" ReturnType="Back" projectId={projectId} userId={userId} />
+                <FlatList
+                    data={projectMembers}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id}
+                />
         </SafeAreaView>
 
     );
