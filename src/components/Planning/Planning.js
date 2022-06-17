90c79af1a842b4b7924a2dc29ad89c535d523cf8
FrancaBaars <franca.baars@student.nhlstenden.com>
2022-06-17 15:00:34 +0200

    Fixed styling planningoverzicht

--- src/components/Planning/Planning.js
+++ src/components/Planning/Planning.js
@@ -10,47 +10,66 @@ export default function Planning({projectId, userId, planningId, week, activitei
 
     return(
 
-        <View>
-            <Text style={Styles.sectionHeader}>Week {week}</Text>
+        <View style={Styles.body}>
+            <View style={Styles.header}>
+                <Text style={Styles.text}>Week {week}</Text>
+            </View>
                 <View style={Styles.row}>
+                    <View style={Styles.activity}>
+                        <Text>{activiteit}</Text>
+                    </View>
+                    <View style={Styles.change}>
+                        <Pressable
+                            onPress={() => navigation.navigate("ScheduleEditScreen", {
+                                projectId,
+                                userId,
+                                planningId,
+                            })}
+                        >   
+                            <FontAwesome style={Styles.icon} name="pencil-square-o" size={24} color="black" />
+                        </Pressable>
 
-                <Text>{activiteit}</Text>
-
-                <Pressable
-                    onPress={() => navigation.navigate("ScheduleEditScreen", {
-                        projectId,
-                        userId,
-                        planningId,
-                    })}
-                >
-                    <FontAwesome style={Styles.icon} name="pencil-square-o" size={24} color="black" />
-                </Pressable>
+                    </View>
             </View>
         </View>
     )
 }
 
 const Styles = StyleSheet.create({
-    row: {
-		flexDirection: 'row', 
-        paddingVertical: 5,
-		backgroundColor: "white",
-		borderWidth: 2,
-        borderRadius: 9,
-        justifyContent: 'center',
-		textAlign: 'left',
-		fontWeight: 'bold',
-		justifyContent: 'space-between',
-		marginBottom: 10,
-		
-
-	},
-	sectionHeader: {
-		backgroundColor: "#009BAA",
-		color: 'white',
-		
-	},
+    body: {
+        flex: 1,
+    },
+    row:{
+        flex: 1,
+        flexDirection: "row",
+        alignSelf: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "90%",
+        minHeight: 50,
+        padding: 10,
+        margin: 8,
+        borderWidth: 3,
+        borderRadius: 20,
+        borderColor: 'white',
+        backgroundColor:'white',
+    },
+    text: {
+        width: "90%",
+        color: 'white',
+        alignSelf: 'center',
+        fontWeight: 'bold',
+    },
 	icon: {
 		textAlign: 'right',
 	},
+    activity: {
+        minWidth: "80%",
+        flexShrink: 1,
+    },
+    change: {
+        minWidth: "5%", 
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
