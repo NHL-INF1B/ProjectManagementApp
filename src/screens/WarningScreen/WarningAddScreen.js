cf6f1a38b8bde9cca26be6f08265d4cac3b78c28
Stefan <stefan.meijer3@student.nhlstenden.com>
2022-06-22 16:35:10 +0200

    Warning add fix

--- src/screens/WarningScreen/WarningAddScreen.js
+++ src/screens/WarningScreen/WarningAddScreen.js
@@ -1,5 +1,5 @@
-import { ScrollView, View, SafeAreaView } from 'react-native';
-import React from 'react';
+import { ScrollView, View, SafeAreaView, TouchableOpacity, Text } from 'react-native';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useForm, Controller } from "react-hook-form";
 import CustomTextInput from '../../components/CustomTextInput/CustomTextInput';
 import styles from './Styles';
@@ -8,24 +8,66 @@ import Circle from '../../components/Circle/Circle';
 import { useNavigation, useRoute } from "@react-navigation/native";
 import CustomButton from '../../components/CustomButton/CustomButton';
 import handlerPath from '../../../env';
+import { Picker, onOpen } from 'react-native-actions-sheet-picker';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const WarningAddScreen = ()  => {
+const WarningAddScreen = () => {
+    const route = useRoute();
+    const project_id = route.params.projectId;
+    const user_id = route.params.userId;
+
+    const [query, setQuery] = useState('');
+    const [members, setMembers] = useState([]); //List of members in project
+    const [selectedMember, setSelectedMember] = useState('Selecteer projectlid'); //Used for putting title of dropdown
 
     const navigation = useNavigation();
 
     const { control, handleSubmit, formState: { errors }, getValues, setValue } = useForm({
         defaultValues: {
-            project_member: "",
             reason: "",
+            userId: "",
         }
     });
 
+    const filteredData = useMemo(() => {
+        if (members && members.length > 0) {
+            return members.filter((item) =>
+                item.name
+                    .toLocaleLowerCase('nl')
+                    .includes(query.toLocaleLowerCase('nl'))
+            );
+        }
+    }, [members, query]);
+
     const submitData = (data) => {
+        console.log(data);
         sendDataToAPI(data);
         navigation.goBack();
         alert("De gegevens zijn opgeslagen");
     };
 
+
+    const getMemberData = () => {
+        try {
+            fetch(handlerPath + "scorebord/scorebord.php", {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    projectId: project_id,
+                }),
+            })
+                .then((response) => response.json())
+                .then((response) => {
+                    console.log(response)
+                    setMembers(response)
+                });
+        } catch (error) {
+            alert(error);
+        }
+    };
 
     // Inserting the data into the database
     const sendDataToAPI = (data) => {
@@ -38,18 +80,18 @@ const WarningAddScreen = ()  => {
                 },
                 body: JSON.stringify({
                     reason: data.reason,
-                    user_id: user_id,
+                    user_id: data.userId,
                     project_id: project_id,
                 }),
             })
-            .then((response) => response.json())
-            .then((response) => {
-                ;
-                setValue("reason", response.reason);
-                setValue("user_id", response.user_id);
-                setValue("project_id", response.project_id);
-                catchFeedback(response);
-            });
+                .then((response) => response.json())
+                .then((response) => {
+                    console.log(response);
+                    setValue("reason", response.reason);
+                    setValue("user_id", response.user_id);
+                    setValue("project_id", response.project_id);
+                    catchFeedback(response);
+                });
         } catch (error) {
             alert(error);
         }
@@ -66,12 +108,8 @@ const WarningAddScreen = ()  => {
             default:
                 //
                 break;
-          }
-	};
-
-    const route = useRoute();
-    const project_id = route.params.projectId;
-    const user_id = route.params.userId;
+        }
+    };
 
     return (
         <SafeAreaView style={styles.SafeAreaView}>
@@ -81,26 +119,41 @@ const WarningAddScreen = ()  => {
                     <Circle name={"alert-circle"} size={60} color={"#000000"} text={"Waarschuwing\nToevoegen"} />
                 </View>
 
-                {/* Project_member */}
-                <View style={styles.marginBottom1}>
+                <View>
                     <Controller
-                        name="project_member"
+                        name="userId"
                         control={control}
                         rules={{
-                            required: { value: true, message: 'Projectlid is verplicht' },
-                            maxLength: {
-                                value: 50,
-                                message: 'Maximaal 50 tekens lang',
-                            }
+                            required: { value: true, message: 'Projectlid selecteren is verplicht' },
                         }}
                         render={({ field: { onChange, value } }) => (
-                            <CustomTextInput 
-                                placeholder="Selecteer een projectlid" 
-                                onChangeText={(text) => onChange(text)} 
-                                value={value} 
-                                errorText={errors?.project_member?.message} 
-                                titleText="Projectlid"
-                            />
+                            <View>
+                                <TouchableOpacity
+                                    style={styles.dropdown}
+                                    onPress={() => {
+                                        getMemberData();
+                                        onOpen('leden');
+                                    }}
+                                >
+                                    <View style={styles.dropdownText}>
+                                        <Text>{selectedMember}</Text>
+                                        <MaterialCommunityIcons name={"chevron-down"} size={20} color={"black"} />
+                                    </View>
+                                </TouchableOpacity>
+
+                                <Picker
+                                    label="Selecteer een projectlid"
+                                    id={'leden'}
+                                    data={filteredData}
+                                    inputValue={query}
+                                    searchable="true"
+                                    placeholderText="Zoek projectlid"
+                                    closeText="Sluiten"
+                                    setSelected={data => { setValue("userId", data.id), setSelectedMember(data.name) }}
+                                />
+
+                                <Text style={{alignSelf:"center", color:"red", fontWeight:"bold"}}>{errors?.userId?.message}</Text>
+                            </View>
                         )}
                     />
                 </View>
@@ -118,11 +171,11 @@ const WarningAddScreen = ()  => {
                             }
                         }}
                         render={({ field: { onChange, value } }) => (
-                            <CustomTextInput 
-                                placeholder="Reden" 
-                                onChangeText={(text) => onChange(text)} 
-                                value={value} 
-                                errorText={errors?.reason?.message} 
+                            <CustomTextInput
+                                placeholder="Reden"
+                                onChangeText={(text) => onChange(text)}
+                                value={value}
+                                errorText={errors?.reason?.message}
                                 titleText="Reden"
                             />
                         )}
@@ -130,7 +183,7 @@ const WarningAddScreen = ()  => {
                 </View>
 
                 <View style={styles.marginBottom5}>
-                    <CustomButton 
+                    <CustomButton
                         buttonType={"redButton"}
                         buttonText={"buttonText"}
                         text={"Toekennen"}
@@ -143,6 +196,6 @@ const WarningAddScreen = ()  => {
 }
 
 export default WarningAddScreen;
-  
+
 
 
